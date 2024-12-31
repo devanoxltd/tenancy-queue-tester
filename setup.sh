14e03d5a1ecf7858069763d060fb014118c33c0b
Samuel Štancl <samuel@archte.ch>
2024-12-31 07:31:37 +0100

    write TENANCY_VERSION to stdout

--- setup.sh
+++ setup.sh
@@ -9,4 +9,6 @@ set -e
 rm -rf src
 chmod -R 777 .
 
+echo "[setup.sh] Tenancy version: ${TENANCY_VERSION}"
+
 docker run --rm -e TENANCY_VERSION="${TENANCY_VERSION}" -v .:/var/www/html tenancy-queue-test-cli bash setup/_tenancy_setup.sh
