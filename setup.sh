eed6fd5bd2479ad4d64493103ff93181116eb68f
Samuel Štancl <samuel@archte.ch>
2025-01-09 17:28:22 +0100

    ci: try adding chmod

--- setup.sh
+++ setup.sh
@@ -12,3 +12,5 @@ chmod -R 777 .
 echo "[setup.sh] Tenancy version: ${TENANCY_VERSION}"
 
 docker run --rm -e TENANCY_VERSION="${TENANCY_VERSION}" -v .:/var/www/html tenancy-queue-test-cli bash setup/_tenancy_setup.sh
+
+chmod -R 777 .
