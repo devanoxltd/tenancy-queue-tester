d261fa1a987a69ef1be972e28d3888d6840ac33b
Samuel Štancl <samuel@archte.ch>
2025-01-09 17:41:34 +0100

    ci: change chmod . to chmod src

--- setup.sh
+++ setup.sh
@@ -11,4 +11,4 @@ chmod -R 777 .
 
 echo "[setup.sh] Tenancy version: ${TENANCY_VERSION}"
 
-docker run --rm -e TENANCY_VERSION="${TENANCY_VERSION}" -v .:/var/www/html tenancy-queue-test-cli bash -c 'setup/_tenancy_setup.sh; chmod -R 777 .'
+docker run --rm -e TENANCY_VERSION="${TENANCY_VERSION}" -v .:/var/www/html tenancy-queue-test-cli bash -c 'setup/_tenancy_setup.sh; chmod -R 777 src'
